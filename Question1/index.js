6adffdbd8c6f96d1e5db252c89b2dac3a738e24d
shinjini03 <81497317+shinjini03@users.noreply.github.com>
2023-07-21 15:22:05 +0530

    Add files via upload

--- Question1/index.js
+++ Question1/index.js
@@ -1,1 +1,39 @@
+const express=require('express');
+const axios=require('axios');
+const index= express();
+const port= 3000;
 
+index.get('/numbers',async(req,res)=> {
+    const { url }= req.query;
+
+    if(!url){
+        return res.status(400).json({ error: 'Please provide at least one url'});
+    }
+    const urls= Array.isArray(url) ? url : [url];
+
+    try{
+        const result= await Promise.all(
+            urls.map(async (link) => {
+                try{
+                    const response = await axios.get(link);
+                    const numbers = extractNumbers(response.data);
+                    return numbers;
+                } catch(error){
+                    return `Error fetching numbers from ${link}: ${error.message}`;
+                }
+            })
+        );
+        res.json({ data:result });
+    } catch(error){
+        res.status(500).json({ error : 'Internal server error'});
+    }
+});
+
+function extractNumbers(htmlContent){
+    const regex = /\d+/g;
+    return htmlContent.match(regex);
+}
+
+index.listen(port,() => {
+    console.log(`Microservice is running on http://localhost:${port}`);
+});
